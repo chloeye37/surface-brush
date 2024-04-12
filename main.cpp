ba9652f561a13eb85468db87e559b7bfcd34fc85
anhduong275 <84537455+anhduong275@users.noreply.github.com>
2024-04-12 16:12:14 -0400

    done rebasing

--- main.cpp
+++ main.cpp
@@ -87,8 +87,8 @@ int main(int argc, char *argv[])
     // std::cout << "Execution took " << duration << " milliseconds." << std::endl;
 
     // Save
-//    m.saveToFile(outStrokeFile.toStdString(), outMeshFile.toStdString());
-    m.debugSaveToFile(outStrokeFile.toStdString(), outMeshFile.toStdString());
+    m.saveToFile(outStrokeFile.toStdString(), outMeshFile.toStdString());
+//    m.debugSaveToFile(outStrokeFile.toStdString(), outMeshFile.toStdString());
     std::cout << "Saved to file." << std::endl;
 
     // Clean up
